2f07decc0a0066254d4ea5887e8e2e51b39e7687
Radwan <radwan@duck.com>
2026-02-04 14:21:27 +0200

    add: csrf trusted origins in settings.py

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,10 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://mradwanz.pythonanywhere.com']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
