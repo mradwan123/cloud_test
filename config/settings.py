ab564fd1bd9ef953f32064f606f9da39e138145f
Radwan <radwan@duck.com>
2026-02-04 12:59:50 +0200

    fix: allowed hosts url

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://mradwanz.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'mradwanz.pythonanywhere.com/']
 
 
 # Application definition
